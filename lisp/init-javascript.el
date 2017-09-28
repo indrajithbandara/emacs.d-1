2254a6c068e123c04a5352712958f214ec7138e6
Ryan Watkins <rwatkins@redant.com>
2017-09-28 15:28:53 +0100

    add: js2-refactor + xref-js2 in init-javascript

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -99,5 +99,19 @@
     (add-hook 'skewer-mode-hook
               (lambda () (inferior-js-keys-mode -1)))))
 
+(require 'js2-refactor)
+(require 'xref-js2)
+
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+
+;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
+;; unbind it.
+(define-key js-mode-map (kbd "M-.") nil)
+(define-key evil-normal-state-map (kbd "M-.") nil)
+(add-hook 'js2-mode-hook (lambda ()
+                           (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+
 
 (provide 'init-javascript)
