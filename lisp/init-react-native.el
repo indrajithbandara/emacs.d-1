6f7dd62f1beed03dfa99e5b9eb65c3f81a9638f5
Ryan Watkins <rwatkins@redant.com>
2017-10-16 16:56:21 +0100

    mod: react-native-packager now has it's own buffer!

--- lisp/init-react-native.el
+++ lisp/init-react-native.el
@@ -1,5 +1,11 @@
 (defun ryan/open-new-packager ()
   (interactive)
-  (shell-command "cd ~/projects/dev/js/mastercard/mastercard-cash-passport/node_modules/react-native/packager && bash launchPackager.command &"))
+  (let ((currentbuf (get-buffer-window (current-buffer)))
+        (newbuf     (generate-new-buffer-name "*react-native-packager*")))
+    (generate-new-buffer newbuf)
+    (set-window-dedicated-p currentbuf nil)
+    (set-window-buffer currentbuf newbuf)
+    (shell-command "cd ~/projects/dev/js/mastercard/mastercard-cash-passport/node_modules/react-native/packager && bash launchPackager.command &" newbuf)))
+
 
 (provide 'init-react-native)
