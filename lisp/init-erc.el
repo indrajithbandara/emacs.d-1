3cba7b6d42174085d0cde2993a9adcaa6ebfc4d4
Ryan Watkins <ryan.watkins@redant.com>
2017-11-26 20:33:06 +0000

    add: irc el function

--- lisp/init-erc.el
+++ lisp/init-erc.el
@@ -1,6 +1,6 @@
 ;; Provide here IRC settings, bitlbee and so forth...
 
-(defun setup-erc ()
+(defun ryan/start-irc ()
   (interactive)
   ;; Auto-connect to channels in the background instead of grabbing buffers...
   (setq erc-join-buffer 'bury)
@@ -8,11 +8,16 @@
         '(("freenode.net" "#archlinux-arm" "#emacs" "#qutebrowser" "#lisp" "#mezzano" "#guix" "#guile" "#raspberrypi")))
   (erc :server "irc.freenode.net" :port 6667 :nick "ryanwatkins"))
 
+(add-hook 'erc-after-connect '(lambda (SERVER NICK)
+                                (erc-message "PRIVMSG" "NickServ identify password")))
 
 (add-hook 'erc-mode-hook
           '(lambda ()
              (define-key erc-mode-map "\C-m" 'newline)
              (define-key erc-mode-map "\C-c\C-c" 'erc-send-current-line)
              ))
+
+(require 'erc-ring)
+(erc-ring-mode t)
 
 (provide 'init-erc)
