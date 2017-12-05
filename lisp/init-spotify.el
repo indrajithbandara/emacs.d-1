c5a68921b744b0019bac9e5de943b41fc924ff11
Ryan Watkins <ryan.watkins@redant.com>
2017-12-05 14:20:26 +0000

    add: spotify-client id/secret (should come from gpg/authinfo though)

--- lisp/init-spotify.el
+++ lisp/init-spotify.el
@@ -1,3 +1,6 @@
 (maybe-require-package 'counsel-spotify)
 
+(setq counsel-spotify-client-id "38938c7fd5e74c7aa58eec072255386e")
+(setq counsel-spotify-client-secret "f2dd310bbcbe4c0bba4dd9c79d1172df")
+
 (provide 'init-spotify)
