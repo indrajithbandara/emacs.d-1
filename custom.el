0b1290c954deb54a18285dfb5d3496056cc26d7e
Ryan Watkins <ryan.watkins@redant.com>
2017-12-09 17:22:38 +0000

    mod: add auto-fetch for molokai theme

--- custom.el
+++ custom.el
@@ -3,12 +3,44 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#839496" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"))
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "c3c0a3702e1d6c0373a0f6a557788dfd49ec9e66e753fb24493579859c8e95ab" default)))
+ '(fci-rule-color "#073642")
  '(package-selected-packages
    (quote
-    (pdf-tools auto-complete-config yari yaml-mode yagist workgroups2 whole-line-or-region whitespace-cleanup-mode wgrep-ag vc-darcs unfill tidy textile-mode tagedit switch-window sql-indent smex smarty-mode slime-company skewer-less session scss-mode scratch sass-mode ruby-hash-syntax rspec-mode robe rinari restclient redshank rainbow-mode rainbow-delimiters projectile-rails project-local-variables pip-requirements php-mode paredit-everywhere page-break-lines origami org-pomodoro org-jira org-fstree org-cliplink nlinum mwe-log-commands move-dup mmm-mode markdown-mode magit-gh-pulls lively less-css-mode ledger-mode json-mode js2-refactor js-comint ipretty intero indent-guide immortal-scratch ibuffer-vc hl-sexp hippie-expand-slime hindent highlight-symbol highlight-quoted highlight-escape-sequences hayoo guide-key grab-mac-link goto-gem go-eldoc gitignore-mode github-issues github-clone gitconfig-mode git-timemachine git-messenger git-blamed general fullframe flycheck-package flycheck-ledger flycheck-elm flycheck-clojure fill-column-indicator expand-region exec-path-from-shell evil-visualstar evil-surround evil-numbers evil-nerd-commenter evil-matchit evil-mark-replace evil-exchange evil-escape erlang ensime elscreen elm-mode elisp-slime-nav elein disable-mouse dired-sort dired+ diminish diff-hl default-text-scale darcsum csv-nav csv-mode css-eldoc counsel company-tern company-quickhelp company-anaconda color-theme coffee-mode cljsbuild-mode cl-lib-highlight cask-mode bundler bug-reference-github browse-kill-ring browse-at-remote avy auto-compile anzu aggressive-indent ag))))
+    (color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized mocha counsel-dash bind-key rg ivy-historian counsel-spotify yasnippet-snippets yasnippets-snippets eyebrowse projectile-ripgrep rjsx-mode org-plus-contrib js2-mode yari yaml-mode yagist workgroups2 whole-line-or-region whitespace-cleanup-mode wgrep-ag w3m vc-darcs unfill tidy textile-mode tagedit switch-window svg sql-indent smex smarty-mode slime-company skewer-less session scss-mode scratch sass-mode ruby-hash-syntax rspec-mode robe rinari regex-tool rainbow-mode rainbow-delimiters projectile-rails project-local-variables pip-requirements php-mode pdf-tools paredit-everywhere page-break-lines osx-location origami org-pomodoro org-jira org-fstree org-cliplink ob-restclient ob-mongo nlinum neotree mwe-log-commands move-dup mmm-mode magithub magit-gh-pulls lua-mode lively less-css-mode ledger-mode jsx-mode json-mode js2-refactor js-doc js-comint ipretty intero inf-mongo indium indent-guide immortal-scratch image+ ibuffer-vc htmlize hl-sexp hippie-expand-slime hindent highlight-symbol highlight-quoted highlight-escape-sequences helm hayoo harvest guide-key goto-gem go-eldoc gnuplot gitignore-mode github-issues github-clone gitconfig-mode git-timemachine git-messenger git-blamed general geiser fullframe flycheck-package flycheck-ledger flycheck-elm flycheck-clojure fill-column-indicator expand-region exec-path-from-shell evil-visualstar evil-surround evil-numbers evil-nerd-commenter evil-matchit evil-mark-replace evil-exchange evil-escape erlang ensime elm-mode elisp-slime-nav elein dsvn disable-mouse dired-sort dired+ diminish diff-hl dictionary default-text-scale darcsum csv-nav csv-mode css-eldoc counsel company-tern company-quickhelp company-anaconda color-theme coffee-mode cljsbuild-mode cl-lib-highlight cask-mode bundler bug-reference-github browse-kill-ring browse-at-remote avy auto-compile anzu aggressive-indent ag)))
+ '(session-use-package t nil (session))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#dc322f")
+     (40 . "#cb4b16")
+     (60 . "#b58900")
+     (80 . "#859900")
+     (100 . "#2aa198")
+     (120 . "#268bd2")
+     (140 . "#d33682")
+     (160 . "#6c71c4")
+     (180 . "#dc322f")
+     (200 . "#cb4b16")
+     (220 . "#b58900")
+     (240 . "#859900")
+     (260 . "#2aa198")
+     (280 . "#268bd2")
+     (300 . "#d33682")
+     (320 . "#6c71c4")
+     (340 . "#dc322f")
+     (360 . "#cb4b16"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#1B1D1E" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "xos4" :family "Terminus (TTF)")))))
--- lisp/init-color-theme.el
+++ lisp/init-color-theme.el
@@ -1,5 +1,28 @@
 (maybe-require-package 'color-theme)
 
+(require 'url)
+
+(defun download-file (&optional url download-dir download-name)
+  (interactive)
+  (let ((url (or url
+                 (read-string "Enter download URL: "))))
+    (let ((download-buffer (url-retrieve-synchronously url)))
+      (save-excursion
+        (set-buffer download-buffer)
+        ;; we may have to trim the http response
+        (goto-char (point-min))
+        (re-search-forward "^$" nil 'move)
+        (forward-char)
+        (delete-region (point-min) (point))
+        (write-file (concat (or download-dir
+                                "~/downloads/")
+                            (or download-name
+                                (car (last (split-string url "/" t))))))))))
+
+(download-file "https://raw.githubusercontent.com/hbin/molokai-theme/master/molokai-theme.el" "~/.emacs.d/themes/" "molokai-theme.el")
+(download-file "https://raw.githubusercontent.com/hbin/molokai-theme/master/molokai-theme-kit.el" "~/.emacs.d/themes/" "molokai-theme-kit.el")
+(setq molokai-theme-kit t)
+
 ;; {{ work around color theme bug
 ;; @see https://plus.google.com/106672400078851000780/posts/KhTgscKE8PM
 (defadvice load-theme (before disable-themes-first activate)
