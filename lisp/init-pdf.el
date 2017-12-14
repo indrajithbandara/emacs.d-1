43f61fef0997352159d95ca626ba3df038246af7
Ryan Watkins <ryan.watkins@redant.com>
2017-12-14 02:30:29 +0000

    add: midnight-mode + amber pdf default

--- lisp/init-pdf.el
+++ lisp/init-pdf.el
@@ -1,4 +1,10 @@
 (maybe-require-package 'pdf-tools)
 (pdf-tools-install)
 
+(add-hook 'pdf-view-mode-hook (lambda ()
+                                (pdf-view-midnight-minor-mode)))
+
+(setq pdf-view-midnight-colors '("#ff9900" . "#0a0a12" ))
+
+
 (provide 'init-pdf)
